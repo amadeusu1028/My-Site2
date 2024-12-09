71e9db0c9dae7b0b9c83b5e7246c64f760be6fae
amadeusu1028 <kobahaya1028@gmail.com>
2024-12-09 15:54:43 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -16,5 +16,19 @@
     centeredSlides: true,
   });
 
+  const open = document.querySelector('#open');
+  const overlay = document.querySelector('.overlay');
+  const close = document.querySelector('#close');
+
+  open.addEventListener('click', () =>{
+    overlay.classList.add('show');
+    open.classList.add('hide');
+  });
+
+  close.addEventListener('click', () =>{
+    overlay.classList.remove('show');
+    open.classList.remove('hide');
+  })
+
   console.log(innerWidth);
 }
