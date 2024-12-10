4cb16d5c7b1a1db9b0ac742ec18725e9f3d0f6ce
amadeusu1028 <kobahaya1028@gmail.com>
2024-12-10 13:21:22 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -31,4 +31,14 @@
   })
 
   console.log(innerWidth);
+
+  $(function(){
+    $('a[href^="#"]').click(function(){
+      let href= $(this).attr("href");
+      let target = $(href == "#" || href == "" ? 'html' : href);
+      let position = target.offset().top;
+      $("html, body").animate({scrollTop:position}, 600, "swing");
+      return false;
+    });
+  })
 }
